d2807805ce61463b7eb1d22214fb54af650f9399
GenkiSato <c0b220732a@edu.teu.ac.jp>
2023-09-26 15:25:12 +0900

    コミット1

---
+++ pygame_image.py
@@ -0,0 +1,29 @@
+import sys
+import pygame as pg
+
+
+def main():
+    pg.display.set_caption("はばたけ！こうかとん")
+    screen = pg.display.set_mode((800, 600))
+    clock  = pg.time.Clock()
+    bg_img = pg.image.load("ex01/fig/pg_bg.jpg")
+    bd_img = pg.image.load("ex01/fig/3.png")
+    bd_img_r = pg.transform.flip(bd_img, True, False)
+    bd_img_10 = 
+    tmr = 0
+    while True:
+        for event in pg.event.get():
+            if event.type == pg.QUIT: return
+
+        screen.blit(bg_img, [0, 0])
+        
+        pg.display.update()
+        tmr += 1        
+        clock.tick(10)
+
+
+if __name__ == "__main__":
+    pg.init()
+    main()
+    pg.quit()
+    sys.exit()
